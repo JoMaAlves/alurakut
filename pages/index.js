4b04bccfc39bf29423de41e4311b84f30422401d
Joao Marcelo Alves <jmarsdalves@gmail.com>
2021-07-13 16:42:27 -0300

    End of Second Day - useState

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-// import styled from 'styled-components'
+import { useEffect, useState } from "react";
 import Box from '../src/components/Box';
 import MainGrid from '../src/components/MainGrid';
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
 
 function ProfileSidebar({ githubUser }) {
   return (
@@ -11,12 +11,28 @@ function ProfileSidebar({ githubUser }) {
         src={`https://github.com/${githubUser}.png`}
         style={{ borderRadius: '8px' }}
       />
+      <hr/>
+      
+      <p>
+        <a className="boxLink" href={`https://github.com/${githubUser}`}>
+          @{githubUser}
+        </a>
+      </p>
+      <hr/>
+
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
   );
 }
 
 export default function Home() {
   const githubUser = 'JoMaAlves';
+  // const [followers, setFollowers] = useState([])
+  const [communities, setCommunities] = useState([{
+    id: new Date().toISOString(),
+    title: 'Eu odeio acordar cedo',
+    image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg'
+  }]);
   const pessoasFavoritas = [
     'juunegreiros',
     'omariosouto',
@@ -25,6 +41,47 @@ export default function Home() {
     'marcobrunodev',
     'felipefialho'
   ];
+
+  const iconsSetup = {
+    recados: 5,
+    fotos: 12,
+    videos: 2,
+    fas: 10,
+    mensagens: 20,
+    confiavel: 3,
+    legal: 3,
+    sexy: 2
+  };
+
+  // useEffect(() => {
+  //   fetch("https://api.github.com/users/JoMaalves/followers")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       data.map((item) => 
+  //         setFollowers((prevItems) => [
+  //           ...prevItems,
+  //           item.login,
+  //         ])
+  //       );
+  //     });
+  // }, []);
+
+  const handleCreateComunity = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    
+
+
+    setCommunities([
+      ...communities,
+      {
+        id: new Date().toISOString(),
+        title: formData.get('title'),
+        image: formData.get('image'),
+      }
+    ]);
+  }
   
   return (
     <>
@@ -38,7 +95,34 @@ export default function Home() {
           <Box>
             <h1 className="title">Bem vindo(a)</h1>
 
-            <OrkutNostalgicIconSet />
+            <OrkutNostalgicIconSet {...iconsSetup}/>
+          </Box>
+          
+          <Box>
+            <h2 className="subTitle">O que você deseja fazer?</h2>
+
+            <form onSubmit={handleCreateComunity} >
+              <div>
+                <input 
+                  placeholder="Qual vai ser o nome da sua comunidade?"
+                  name="title"
+                  aria-label="Qual vai ser o nome da sua comunidade?"
+                  type="text"
+                />
+              </div>
+              
+              <div>
+                <input 
+                  placeholder="Coloque uma URL para usarmos de capa"
+                  name="image"
+                  aria-label="Coloque uma URL para usarmos de capa"
+                />
+              </div>
+
+              <button>
+                Criar comunidade
+              </button>
+            </form>
           </Box>
         </div>
 
@@ -52,10 +136,30 @@ export default function Home() {
               {
                 pessoasFavoritas.map((itemAtual) => {
                   return (
-                    <li>
-                      <a href={`/users/${itemAtual}`} key={itemAtual}>
+                    <li key={itemAtual}>
+                      <a href={`/users/${itemAtual}`}>
                         <img src={`https://github.com/${itemAtual}.png`} />
                         <span>{itemAtual}</span>
+                      </a>
+                    </li>
+                  );
+                })
+              }
+            </ul>
+          </ProfileRelationsBoxWrapper>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Comunidades ({communities.length})
+            </h2>
+
+            <ul>
+              {
+                communities.map((itemAtual) => {
+                  return (
+                    <li key={itemAtual.id}>
+                      <a href={`/users/${itemAtual}`}>
+                        <img src={itemAtual.image} />
+                        <span>{itemAtual.title}</span>
                       </a>
                     </li>
                   );
